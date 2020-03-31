0a7f2a1acd161b52c64139741c0f21170f18679e
Artem Vasenin <vasart169@gmail.com>
2020-04-01 01:06:44 +0300

    refactor[install_standard.sh]: Add conditional for git-lfs install

--- .bin/install_standard.sh
+++ .bin/install_standard.sh
@@ -2,6 +2,7 @@
 
 sudo add-apt-repository ppa:mmstick76/alacritty -n -y
 sudo add-apt-repository ppa:regolith-linux/release -n -y
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 sudo apt-get update
 xargs sudo apt-get install -y < $HOME/.local/share/apt_install.txt
 
@@ -17,15 +18,10 @@ if command -v pip > /dev/null; then
 	pip install -r $HOME/.local/share/pip_install.txt
 fi
 
-# Install node (for nvim-coc)
-curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
 # Install neovim
 if [ ! -f "$HOME/.bin/nvim.appimage" ]; then
 	wget "https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage" -O "$HOME/.bin/nvim.appimage" && chmod u+x "$HOME/.bin/nvim.appimage"
 fi
-pip install neovim
 sudo npm install -g neovim
 
 # Install Vim plugins
@@ -43,13 +39,15 @@ cd "$HOME/.local/share/fzf"
 yes | ./install
 cd -
 
+# Install git lfs
+if ! git lfs &>/dev/null; then
+	cd /tmp
+	wget "https://github.com/git-lfs/git-lfs/releases/download/v2.10.0/git-lfs-linux-amd64-v2.10.0.tar.gz" -O "git-lfs.tar.gz" &&
+		tar -xzf git-lfs.tar.gz &&
+		sudo ./install.sh &&
+		git lfs install
+	cd -
+fi
+
 # Update config
 config pull
-
-# Install git lfs
-cd /tmp
-wget "https://github.com/git-lfs/git-lfs/releases/download/v2.10.0/git-lfs-linux-amd64-v2.10.0.tar.gz" -O "git-lfs.tar.gz" &&
-	tar -xzf git-lfs.tar.gz &&
-	sudo ./install.sh &&
-	git lfs install
-cd -
