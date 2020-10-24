5aba82be6c157b95a2ce60c993bb4d6b07cbc115
Artem Vasenin <vasart169@gmail.com>
2020-10-24 22:19:11 +0300

    chore: Minor changes

--- .bin/find_monitors.sh
+++ .bin/find_monitors.sh
@@ -2,6 +2,6 @@
 # Create config file with monitors
 find_monitors() {
 	IFS=' '; numbers=($(xrandr | grep "DP.* connected" | cut -d ' ' -f1 | tr '\n' ' '))
-	printf "monitor_left: ${numbers[2]}\nmonitor_right: ${numbers[1]}" > .local/share/monitors.yaml
+	printf "monitor_left: ${numbers[0]}\nmonitor_right: ${numbers[1]}" > .local/share/monitors.yaml
 }
 find_monitors
