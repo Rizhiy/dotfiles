b1f65de3c0d0ca6a5cd5973865e6ce326e0647a2
Artem Vasenin <vasart169@gmail.com>
2020-10-28 03:46:42 +0300

    feat: Add keyboard layout to lock screen

--- .bin/lock.sh
+++ .bin/lock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-. "$HOME/.local/share/multilockscreen/multilockscreen" --lock
+. "$HOME/.local/share/multilockscreen/multilockscreen" --lock -- --layoutpos='ix-90:iy-0' --layout-align 1 --layoutcolor=ffffffff --keylayout 2
