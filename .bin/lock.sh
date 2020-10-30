51e8b94f99f03aae4b135386a3e9f73f70675155
Artem Vasenin <vasart169@gmail.com>
2020-10-30 14:43:27 +0300

    chore[lock.sh]: Force immediate monitor sleep

--- .bin/lock.sh
+++ .bin/lock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-. "$HOME/.local/share/multilockscreen/multilockscreen" --lock -- --layoutpos='ix-90:iy-0' --layout-align 1 --layoutcolor=ffffffff --keylayout 2
+. "$HOME/.local/share/multilockscreen/multilockscreen" --lock -- --layoutpos='ix-90:iy-0' --layout-align 1 --layoutcolor=ffffffff --keylayout 2 & sleep 1 && xset dpms force off
