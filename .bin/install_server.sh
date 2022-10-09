4816ec591ed41958ff67dc070e748c5d1ba101ae
Artem Vasenin <rizhiy@ip-172-31-6-230.ec2.internal>
2022-10-09 23:53:33 +0000

    imp(lazygit): Add installation back and improve config

--- .bin/install_server.sh
+++ .bin/install_server.sh
@@ -96,3 +96,9 @@ if ! command -b "bat" > /dev/null; then
 	sudo dpkg -i "$bat_path"
 	rm -fr "$bat_path"
 fi
+
+# Lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"v*([^"]+)".*/\1/')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit
+rm lazygit.tar.gz
