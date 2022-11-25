8a920fec6ad12c2c43f8031436f033acaa438068
Artem Vasenin <rizhiy@ip-172-31-28-48.ec2.internal>
2022-11-25 18:47:49 +0000

    fix(install_server.sh): Fix zsh install

--- .bin/install_server.sh
+++ .bin/install_server.sh
@@ -80,7 +80,7 @@ fi
 
 # Install oh-my-zsh
 if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 # Change default shell to zsh
 sudo usermod -s $(which zsh) $(whoami)
