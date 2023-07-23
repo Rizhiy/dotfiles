5b826eb730eb48dcc4d6558dbd12c862ec27b84e
Artem Vasenin <vasart169@gmail.com>
2023-07-23 13:55:06 +0100

    fix(.tmux.conf): Potential fix for uberzug not working inside tmux

--- .bin/install_standard.sh
+++ .bin/install_standard.sh
@@ -10,6 +10,9 @@ sudo add-apt-repository ppa:aslatter/ppa -n -y
 /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2023.02.18_all.deb keyring.deb SHA256:a511ac5f10cd811f8a4ca44d665f2fa1add7a9f09bef238cdfad8461f5239cc4
 sudo apt install ./keyring.deb
 echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
+# Ueberzugpp - image viewer
+echo 'deb http://download.opensuse.org/repositories/home:/justkidding/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/home:justkidding.list
+curl -fsSL https://download.opensuse.org/repositories/home:justkidding/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_justkidding.gpg > /dev/null
 
 if ! command -v "google-chrome-stable"; then
 	wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
