bc0002aff8fb6530b18d2f188e2893dba145f34b
Artem Vasenin <vasart169@gmail.com>
2023-09-06 02:37:55 +0100

    fix(install_server.sh): Fix nodejs installation

--- .bin/install_server.sh
+++ .bin/install_server.sh
@@ -5,7 +5,11 @@ set -e
 
 # Always have latest git version
 sudo add-apt-repository ppa:git-core/ppa -n -y
-curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+# Add node ppa
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 if [ ! -d "$HOME/miniconda3" ]; then
 	wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "$HOME/miniconda3.sh"
