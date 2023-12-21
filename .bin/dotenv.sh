f176c5635567a7b645c22bffdc478d30bf114912
Artem Vasenin <avasenin@amazon.co.uk>
2023-12-21 15:06:50 +0000

    feat(dotenv.sh): Improve script

--- .bin/dotenv.sh
+++ .bin/dotenv.sh
@@ -1,5 +1,13 @@
-#!/usr/bin/env bash
-if [ ! -f .env ]
-then
-  export $(cat .env | xargs)
+local filename="${1:-.env}"
+
+if [ ! -f "$filename" ]; then
+    echo "Missing ${filename} file!"
+    exit 1
 fi
+
+echo "Reading .env file..."
+while read -r LINE; do
+    if [[ $LINE != '#'* ]] && [[ $LINE == *'='* ]]; then
+        export "$LINE"
+    fi
+done < "$filename"
