ff160444637aad94d31ce42d6d931e4b37e58fac
Artem Vasenin <vasart169@gmail.com>
2023-12-24 03:57:32 +0000

    chore(nvim): Disable screensaver for now

--- .config/nvim/lua/plugins/zone.lua
+++ .config/nvim/lua/plugins/zone.lua
@@ -1,8 +1,9 @@
 return {
     "tamton-aquib/zone.nvim",
+    enabled = false,
     opts = {
         style = "vanish",
         after = 60,
-        exclude_filetype = { "TelescopePrompt", "NvimTree", "neo-tree", "dashboard", "lazy", "lua" },
+        exclude_filetype = { "TelescopePrompt", "NvimTree", "neo-tree", "dashboard", "lazy" },
     },
 }
