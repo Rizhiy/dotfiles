a17d2cb6c1e078fbcd91e381e6092bc5ffcf5343
Artem Vasenin <vasart169@gmail.com>
2023-12-27 00:59:55 +0000

    feat(nvim): Add registers plugin

---
+++ .config/nvim/lua/plugins/registers.lua
@@ -0,0 +1,13 @@
+return {
+    "tversteeg/registers.nvim",
+    name = "registers",
+    keys = {
+        { '"', mode = { "n", "v" } },
+        { "<C-R>", mode = "i" },
+    },
+    cmd = "Registers",
+    opts = { window = {
+        border = require("rizhiy.border").Border(),
+        transparency = 0,
+    } },
+}
