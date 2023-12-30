fedbd93fecad4bc09495160e06b799009cfe397d
Artem Vasenin <vasart169@gmail.com>
2023-12-30 04:29:55 +0000

    feat(nvim): Add desc to floaterm keys

--- .config/nvim/lua/plugins/floatterm.lua
+++ .config/nvim/lua/plugins/floatterm.lua
@@ -1,7 +1,10 @@
 return {
     "voldikss/vim-floaterm",
     keys = {
-        { "<F1>", "<ESC>:FloatermToggle<CR>", mode = { "n", "v", "i" } },
+        { "<F1>",      "<ESC>:FloatermToggle<CR>", mode = { "n", "v", "i" }, desc = "Toggle terminal" },
+        { "<F2>",      "<ESC>:FloatermPrev<CR>",   mode = { "n", "v", "i" }, desc = "Prev terminal" },
+        { "<F3>",      "<ESC>:FloatermNext<CR>",   mode = { "n", "v", "i" }, desc = "Next terminal" },
+        { "<F4>",      "<ESC>:FloatermNew<CR>",    mode = { "n", "v", "i" }, desc = "New terminal" },
         { "<leader>g", ":FloatermNew lazygit<CR>", desc = "LazyGit" },
     },
     init = function()
