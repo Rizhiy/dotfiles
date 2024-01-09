5f65a690e6ca80e41ff57904bcd68555610a9753
Artem Vasenin <vasart169@gmail.com>
2024-01-09 10:50:48 +0000

    chore(nvim): Remove unnecessary options

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -7,15 +7,12 @@ return {
                 require("Comment.api").toggle.linewise.current()
                 require("rizhiy.keys").press("j", "n")
             end,
-            mode = "n",
-            silent = true,
             desc = "Comment line",
         },
         {
             "<C-_>",
             function() require("Comment.api").toggle.linewise(vim.fn.visualmode()) end,
             mode = "v",
-            silent = true,
             desc = "Comment selection",
         },
     },
