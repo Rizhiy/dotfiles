725861f1bd16a40fe9ceff5a1c2e896a2c43edbd
Artem Vasenin <avasenin@amazon.co.uk>
2024-01-11 11:42:06 +0000

    chore(nvim): Improve split/tmux navigation

--- .config/nvim/lua/plugins/tmux.lua
+++ .config/nvim/lua/plugins/tmux.lua
@@ -1,13 +1,13 @@
+function keymap(lhs, rhs, desc) return { lhs, rhs, desc = desc, mode = { "n", "v", "i", "x" }, silent = true } end
+
 return {
     {
-
         "christoomey/vim-tmux-navigator",
         keys = {
-            { "<M-h>", ":<C-U>TmuxNavigateLeft<CR>", desc = "Move Left", silent = true },
-            { "<M-j>", ":<C-U>TmuxNavigateDown<CR>", desc = "Move Down", silent = true },
-            { "<M-k>", ":<C-U>TmuxNavigateUp<CR>", desc = "Move Up", silent = true },
-            { "<M-l>", ":<C-U>TmuxNavigateRight<CR>", desc = "Move Right", silent = true },
-            { "<M-\\>", ":<C-U>TmuxNavigateDown<CR>", desc = "Move Down", silent = true },
+            keymap("<M-h>", "<ESC>:<C-U>TmuxNavigateLeft<CR>", "Move Left"),
+            keymap("<M-j>", "<ESC>:<C-U>TmuxNavigateDown<CR>", "Move Down"),
+            keymap("<M-k>", "<ESC>:<C-U>TmuxNavigateUp<CR>", "Move Up"),
+            keymap("<M-l>", "<ESC>:<C-U>TmuxNavigateRight<CR>", "Move Right"),
         },
         init = function()
             vim.cmd("let g:tmux_navigator_save_on_switch = 2") -- Write all buffers before navigating from Vim to tmux pane
