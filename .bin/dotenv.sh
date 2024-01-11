d1ac595edce361bbe4bad750c63b0c0ed76d1476
Artem Vasenin <avasenin@amazon.co.uk>
2024-01-11 13:51:58 +0000

    feat(dotenv.sh): Add check that script is being sourced

--- .bin/dotenv.sh
+++ .bin/dotenv.sh
@@ -1,3 +1,20 @@
+sourced=0
+if [ -n "$ZSH_VERSION" ]; then
+  case $ZSH_EVAL_CONTEXT in *:file) sourced=1;; esac
+elif [ -n "$KSH_VERSION" ]; then
+  [ "$(cd -- "$(dirname -- "$0")" && pwd -P)/$(basename -- "$0")" != "$(cd -- "$(dirname -- "${.sh.file}")" && pwd -P)/$(basename -- "${.sh.file}")" ] && sourced=1
+elif [ -n "$BASH_VERSION" ]; then
+  (return 0 2>/dev/null) && sourced=1
+else # All other shells: examine $0 for known shell binary filenames.
+     # Detects `sh` and `dash`; add additional shell filenames as needed.
+  case ${0##*/} in sh|-sh|dash|-dash) sourced=1;; esac
+fi
+
+if [ $sourced -eq 0 ]; then
+    echo "This script has to be sourced to work properly!"
+    exit 1
+fi
+
 filename="${1:-.env}"
 
 if [ ! -f "$filename" ]; then
