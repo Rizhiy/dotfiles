c35adf222d8fd3e8391227a9cedab4fac274da6f
Artem Vasenin <vasart169@gmail.com>
2024-01-13 14:27:38 +0000

    chore(nvim): Remove dead code

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -18,34 +18,6 @@ return {
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
 
-        local handler = function(virtText, lnum, endLnum, width, truncate)
-            local newVirtText = {}
-            local suffix = (" 󰁂 %d "):format(endLnum - lnum)
-            local sufWidth = vim.fn.strdisplaywidth(suffix)
-            local targetWidth = width - sufWidth
-            local curWidth = 0
-            for _, chunk in ipairs(virtText) do
-                local chunkText = chunk[1]
-                local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-                if targetWidth > curWidth + chunkWidth then
-                    table.insert(newVirtText, chunk)
-                else
-                    chunkText = truncate(chunkText, targetWidth - curWidth)
-                    local hlGroup = chunk[2]
-                    table.insert(newVirtText, { chunkText, hlGroup })
-                    chunkWidth = vim.fn.strdisplaywidth(chunkText)
-                    -- str width returned from truncate() may less than 2nd argument, need padding
-                    if curWidth + chunkWidth < targetWidth then
-                        suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
-                    end
-                    break
-                end
-                curWidth = curWidth + chunkWidth
-            end
-            table.insert(newVirtText, { suffix, "MoreMsg" })
-            return newVirtText
-        end
-
         local handler = function(
             virtual_text_chunks, -- The start_line's text.
             start_line,          -- Start lines of fold.
@@ -109,7 +81,6 @@ return {
                 ufo.closeFoldsWith(1)
             end)
         end
-
         vim.api.nvim_create_autocmd("BufRead", {
             pattern = "*",
             callback = function(e) applyFoldsAndThenCloseAllFolds(e.buf, "treesitter") end,
