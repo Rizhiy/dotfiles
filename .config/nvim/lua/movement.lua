5074ba846978d724b4f1815b37c09162654ee5cc
Artem Vasenin <vasart169@gmail.com>
2024-01-13 14:15:29 +0000

    chore(nvim): Update screen movement

--- .config/nvim/lua/movement.lua
+++ .config/nvim/lua/movement.lua
@@ -2,8 +2,8 @@ local nmap = require("rizhiy.keys").nmap
 nmap("gb", "<C-o>", { desc = "Go to previous location", noremap = true })
 nmap("gf", "<C-i>", { desc = "Go to next location", noremap = true })
 
-nmap("<C-j>", "<C-d>zz", { desc = "Move screen down" })
-nmap("<C-k>", "<C-u>zz", { desc = "Move screen up" })
+nmap("<C-j>", "<C-e>j", { desc = "Move screen down" })
+nmap("<C-k>", "<C-y>k", { desc = "Move screen up" })
 
 for idx = 1, 9 do
     nmap("<leader>" .. idx, idx .. "<C-w>w", { desc = "Move to window " .. idx })
