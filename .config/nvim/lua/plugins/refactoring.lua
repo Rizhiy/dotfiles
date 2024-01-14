b0c6d8f7ee14b943b416f9e965addeafc963171e
Artem Vasenin <vasart169@gmail.com>
2024-01-14 14:03:56 +0000

    feat(nvim): Add basic refactoring

---
+++ .config/nvim/lua/plugins/refactoring.lua
@@ -0,0 +1,24 @@
+return {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {},
+    keys = {
+        -- Extract function supports only visual mode
+        {
+            "<leader>re",
+            function() require("refactoring").refactor("Extract Function") end,
+            desc = "Extract Function",
+            mode = "x",
+        },
+        -- Extract variable supports only visual mode
+        {
+            "<leader>rv",
+            function() require("refactoring").refactor("Extract Variable") end,
+            desc = "Extract Variable",
+            mode = "x",
+        },
+    },
+}
