9873f8d59744fee4cb37ca468b9abe4f92b3c825
Artem Vasenin <vasart169@gmail.com>
2024-01-15 00:49:38 +0000

    feat(nvim): Include tabline when multiple tabs are open

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,13 +1,48 @@
 local function window() return vim.api.nvim_win_get_number(0) end
 return {
     "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
-    opts = {
-        inactive_sections = {
-            lualine_y = { window },
-        },
-        options = {
-            theme = "gruvbox-material",
-        },
-    },
+    dependencies = { { "nvim-tree/nvim-web-devicons", opt = true }, "folke/noice.nvim" },
+    config = function()
+        local noice = require("noice")
+        require("lualine").setup({
+            sections = {
+                lualine_c = {
+                    { -- filename relative to cwd
+                        "filename",
+                        path = 1,
+                    },
+                    { -- search count
+                        noice.api.status.search.get,
+                        cond = require("noice").api.status.search.has,
+                        color = { fg = "#ff9e64" },
+                    },
+                },
+                lualine_x = {
+                    { -- Show when macro is being recorded
+                        noice.api.statusline.mode.get,
+                        cond = noice.api.statusline.mode.has,
+                        color = { fg = "#ff9e64" },
+                    },
+                    -- Original sections
+                    "encoding",
+                    "fileformat",
+                    "filetype",
+                },
+            },
+            inactive_sections = {
+                lualine_y = { window },
+            },
+            options = {
+                theme = "gruvbox-material",
+            },
+            tabline = { lualine_a = { { "tabs", mode = 2, path = 1 } } },
+            extensions = {
+                "mason",
+                "lazy",
+                "quickfix",
+                "trouble",
+            },
+        })
+        vim.cmd("set showtabline=1")
+    end,
 }
--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -12,11 +12,11 @@ return {
         },
         -- you can enable a preset for easier configuration
         presets = {
-            bottom_search = true, -- use a classic bottom cmdline for search
-            command_palette = true, -- position the cmdline and popupmenu together
+            bottom_search = true,         -- use a classic bottom cmdline for search
+            command_palette = true,       -- position the cmdline and popupmenu together
             long_message_to_split = true, -- long messages will be sent to a split
-            inc_rename = false, -- enables an input dialog for inc-rename.nvim
-            lsp_doc_border = true, -- add a border to hover docs and signature help
+            inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+            lsp_doc_border = true,        -- add a border to hover docs and signature help
         },
         messages = {
             view = "mini",
@@ -59,33 +59,4 @@ return {
             },
         },
     },
-    init = function()
-        local noice = require("noice")
-        require("lualine").setup({
-            sections = {
-                lualine_c = {
-                    { -- filename relative to cwd
-                        "filename",
-                        path = 1,
-                    },
-                    { -- search count
-                        noice.api.status.search.get,
-                        cond = require("noice").api.status.search.has,
-                        color = { fg = "#ff9e64" },
-                    },
-                },
-                lualine_x = {
-                    { -- Show when macro is being recorded
-                        noice.api.statusline.mode.get,
-                        cond = noice.api.statusline.mode.has,
-                        color = { fg = "#ff9e64" },
-                    },
-                    -- Original sections
-                    "encoding",
-                    "fileformat",
-                    "filetype",
-                },
-            },
-        })
-    end,
 }
