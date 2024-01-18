d070560de89fdb9dcb420fbb15a2cb164509dfb9
Artem Vasenin <avasenin@amazon.co.uk>
2024-01-18 17:12:42 +0000

    chore(nvim): Minor adjustments

--- .config/nvim/lua/plugins/tmux.lua
+++ .config/nvim/lua/plugins/tmux.lua
@@ -10,7 +10,8 @@ return {
             keymap("<M-l>", "<ESC>:<C-U>TmuxNavigateRight<CR>", "Move Right"),
         },
         init = function()
-            vim.cmd("let g:tmux_navigator_save_on_switch = 2") -- Write all buffers before navigating from Vim to tmux pane
+            vim.g.tmux_navigator_save_on_switch = 2 -- Write all buffers before navigating from Vim to tmux pane
+            vim.g.tmux_navigator_no_mappings = 1
         end,
     },
     { "tmux-plugins/vim-tmux" },
