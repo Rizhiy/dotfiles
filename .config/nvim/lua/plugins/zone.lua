5111fcdf3f89329ed80da25a4dfba8e375208235
Artem Vasenin <vasart169@gmail.com>
2024-02-03 01:44:30 +0000

    chore(nvim): Remove unused plugin

--- .config/nvim/lua/plugins/zone.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "tamton-aquib/zone.nvim",
-    enabled = false,
-    opts = {
-        style = "vanish",
-        after = 60,
-        exclude_filetype = { "TelescopePrompt", "NvimTree", "neo-tree", "dashboard", "lazy" },
-    },
-}
