2188d9677b6bfb5f156ac0bef7c3c6c6087e668a
Artem Vasenin <vasart169@gmail.com>
2024-03-19 23:37:44 +0000

    imp(nvim): Make oil respect .gitignore

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,31 +1,60 @@
+local function get_git_ignored_files_in(dir)
+    local cmd =
+        string.format('git -C %s ls-files --ignored --exclude-standard --others --directory | grep -v "/.*\\/"', dir)
+
+    local handle = io.popen(cmd)
+    if handle == nil then return end
+
+    local ignored_files = {}
+    for line in handle:lines("*l") do
+        line = line:gsub("/$", "")
+        table.insert(ignored_files, line)
+    end
+    handle:close()
+
+    return ignored_files
+end
+
 return {
     "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+        { "-", ":Oil<CR>", desc = "Open parent directory" },
+    },
     opts = {
         keymaps = {
             ["?"] = "actions.show_help",
+
             ["<CR>"] = "actions.select",
+
             ["v"] = "actions.select_vsplit",
             ["-"] = "actions.select_split",
+
             ["<C-t>"] = "actions.select_tab",
             ["<C-p>"] = "actions.preview",
             ["<C-d>"] = "actions.close",
             ["<C-r>"] = "actions.refresh",
+
             ["<BS>"] = "actions.parent",
-            ["~"] = "actions.open_cwd",
+            ["_"] = "actions.open_cwd",
+
             ["gs"] = "actions.change_sort",
             ["gx"] = "actions.open_external",
             ["g."] = "actions.toggle_hidden",
             ["g\\"] = "actions.toggle_trash",
         },
-        use_default_keymaps = true,
+        use_default_keymaps = false,
         columns = {
             "mtime",
             "size",
             "icon",
         },
-    },
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    keys = {
-        { "-", ":Oil<CR>", desc = "Open parent directory" },
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+            is_hidden_file = function(name, _)
+                local ignored_files = get_git_ignored_files_in(require("oil").get_current_dir())
+                return vim.tbl_contains(ignored_files, name) or vim.startswith(name, ".")
+            end,
+        },
     },
 }
