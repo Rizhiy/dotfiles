3cd59a1ef3edcee67a0c446da20fee430a4fa3c7
Artem Vasenin <vasart169@gmail.com>
2024-04-17 22:03:31 +0100

    fix(reload_nvidia.sh): Add check for root user

--- .bin/reload_nvidia.sh
+++ .bin/reload_nvidia.sh
@@ -1,4 +1,8 @@
 # Stop graphical interface
+if [ $(id -u ) -ne 0 ]; then
+    echo "This script needs to be run as sudo!"
+    exit
+fi
 systemctl isolate multi-user.target
 # Unload all nvidia modules
 modprobe -r nvidia_uvm
