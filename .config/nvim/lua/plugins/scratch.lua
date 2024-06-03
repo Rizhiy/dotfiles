925badcb7e68f288873b20cfbfb327529965c2ea
Artem Vasenin <rizhiy@meta.com>
2024-06-03 10:25:13 +0100

    fix(nvim): properly remove codi

--- .config/nvim/lua/plugins/scratch.lua
+++ .config/nvim/lua/plugins/scratch.lua
@@ -1,15 +1,9 @@
 return {
     "LintaoAmons/scratch.nvim",
-    dependencies = { "metakirby5/codi.vim" },
     keys = {
-        { "<leader>sc", ":Scratch<CR>",           desc = "Create new ScratchPad",       silent = true },
-        { "<leader>sn", ":ScratchWithName<CR>",   desc = "Create ScratchPad with name", silent = true },
-        { "<leader>se", ":ScratchEditConfig<CR>", desc = "Edit ScratchPad config",      silent = true },
+        { "<leader>sc", ":Scratch<CR>", desc = "Create new ScratchPad", silent = true },
+        { "<leader>sn", ":ScratchWithName<CR>", desc = "Create ScratchPad with name", silent = true },
+        { "<leader>se", ":ScratchEditConfig<CR>", desc = "Edit ScratchPad config", silent = true },
+        { "<leader>sf", ":ScratchOpen<CR>", desc = "Find existing scratchpad", silent = true },
     },
-    config = function()
-        vim.api.nvim_create_autocmd({ "BufEnter" }, {
-            pattern = "*/scratch.nvim/*",
-            command = "Codi",
-        })
-    end,
 }
