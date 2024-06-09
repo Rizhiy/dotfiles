2e26bbc4ce43a9f16809b46d1d66b7962cd4b163
Artem Vasenin <vasart169@gmail.com>
2024-06-09 23:24:12 +0100

    fix(nvim): update scratch config

--- .config/nvim/lua/plugins/scratch.lua
+++ .config/nvim/lua/plugins/scratch.lua
@@ -3,7 +3,9 @@ return {
     keys = {
         { "<leader>sc", ":Scratch<CR>", desc = "Create new ScratchPad", silent = true },
         { "<leader>sn", ":ScratchWithName<CR>", desc = "Create ScratchPad with name", silent = true },
-        { "<leader>se", ":ScratchEditConfig<CR>", desc = "Edit ScratchPad config", silent = true },
         { "<leader>sf", ":ScratchOpen<CR>", desc = "Find existing scratchpad", silent = true },
+    },
+    opts = {
+        filetypes = { "py", "lua" },
     },
 }
