d825ae486666f96f1f42b2480362886eaf07b887
Artem Vasenin <rizhiy@meta.com>
2024-06-24 18:11:31 +0100

    chore(nvim): remove gd, use default <C-]> instead

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -9,7 +9,6 @@ local on_attach = function(_, bufnr)
     nmap("rn", vim.lsp.buf.rename, "[R]e[n]ame")
     nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
-    nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
     nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
     nmap("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
     nmap("<leader>d", function() vim.diagnostic.open_float({ scope = "cursor" }) end, "Show diagnostics under cursor")
