8d1f05580caad91939da80ec1b987acf9d525d9b
Artem Vasenin <rizhiy@meta.com>
2024-06-24 10:14:43 +0100

    chore(nvim): add json and txt to scratch

--- .config/nvim/lua/plugins/scratch.lua
+++ .config/nvim/lua/plugins/scratch.lua
@@ -1,11 +1,11 @@
 return {
     "LintaoAmons/scratch.nvim",
     keys = {
-        { "<leader>sc", ":Scratch<CR>", desc = "Create new ScratchPad", silent = true },
+        { "<leader>sc", ":Scratch<CR>",         desc = "Create new ScratchPad",       silent = true },
         { "<leader>sn", ":ScratchWithName<CR>", desc = "Create ScratchPad with name", silent = true },
-        { "<leader>sf", ":ScratchOpen<CR>", desc = "Find existing scratchpad", silent = true },
+        { "<leader>sf", ":ScratchOpen<CR>",     desc = "Find existing scratchpad",    silent = true },
     },
     opts = {
-        filetypes = { "py", "lua" },
+        filetypes = { "py", "lua", "txt", "json" },
     },
 }
