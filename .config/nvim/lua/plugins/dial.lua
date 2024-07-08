2c0d83831e6fed957eb455d5c8fe9d99fe18491b
Artem Vasenin <vasart169@gmail.com>
2024-07-09 00:30:59 +0100

    style(nvim): format

--- .config/nvim/lua/plugins/dial.lua
+++ .config/nvim/lua/plugins/dial.lua
@@ -1,10 +1,10 @@
 return {
     "monaqa/dial.nvim",
     keys = {
-        { "<C-a>",  function() require("dial.map").manipulate("increment", "normal") end },
-        { "<C-x>",  function() require("dial.map").manipulate("decrement", "normal") end },
-        { "<C-a>",  function() require("dial.map").manipulate("increment", "visual") end,  mode = { "v" } },
-        { "<C-x>",  function() require("dial.map").manipulate("decrement", "visual") end,  mode = { "v" } },
+        { "<C-a>", function() require("dial.map").manipulate("increment", "normal") end },
+        { "<C-x>", function() require("dial.map").manipulate("decrement", "normal") end },
+        { "<C-a>", function() require("dial.map").manipulate("increment", "visual") end, mode = { "v" } },
+        { "<C-x>", function() require("dial.map").manipulate("decrement", "visual") end, mode = { "v" } },
         { "g<C-a>", function() require("dial.map").manipulate("increment", "gvisual") end, mode = { "v" } },
         { "g<C-x>", function() require("dial.map").manipulate("decrement", "gvisual") end, mode = { "v" } },
     },
