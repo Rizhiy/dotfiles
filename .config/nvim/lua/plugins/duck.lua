53984112382617991411f50f7773c7ce7bc588cd
Artem Vasenin <rizhiy@meta.com>
2024-07-09 17:53:23 +0100

    chore(nvim): remove duck, since not using it

--- .config/nvim/lua/plugins/duck.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "tamton-aquib/duck.nvim",
-    keys = {
-        { "<leader>oD", function() require("duck").hatch() end, desc = "Release the duck" },
-        { "<leader>oC", function() require("duck").cook() end,  desc = "Catch the duck" },
-    },
-}
