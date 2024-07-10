68cc4de7b6df8c538c1f8c1eb7b2cc70ff488235
Artem Vasenin <vasart169@gmail.com>
2024-07-10 23:00:14 +0100

    feat(nvim): add sleuth

--- .config/nvim/lua/general.lua
+++ .config/nvim/lua/general.lua
@@ -1,11 +1,8 @@
 vim.opt.fileencoding = "utf-8"
 
--- NOTE: Maybe use 'tpope/vim-sleuth'?
-vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.smartindent = true
 
 vim.opt.spell = true
 vim.opt.spelllang = "en_gb,en_us"
