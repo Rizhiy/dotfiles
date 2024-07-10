f6f2ee13c414e32d9e787ca79cb30ffba6db98ee
Artem Vasenin <vasart169@gmail.com>
2024-07-10 23:21:33 +0100

    feat(nvim): add ability to jump by buffer

---
+++ .config/nvim/lua/plugins/bufjump.lua
@@ -0,0 +1,8 @@
+return {
+    "kwkarlwang/bufjump.nvim",
+    keys = {
+        { "]b", function() require("bufjump").forward() end, desc = "Go to next buffer", silent = true },
+        { "[b", function() require("bufjump").backward() end, desc = "Go to previous buffer", silent = true },
+    },
+    opts = true,
+}
