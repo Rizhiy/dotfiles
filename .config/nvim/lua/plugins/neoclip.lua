fa2ef610ffd8f43d94b5418fefd299a52b0317ec
Artem Vasenin <vasart169@gmail.com>
2024-07-15 21:38:08 +0100

    feat(nvim): add plugin to keep history of yanks and macros

---
+++ .config/nvim/lua/plugins/neoclip.lua
@@ -0,0 +1,24 @@
+return {
+    "AckslD/nvim-neoclip.lua",
+    dependencies = {
+        { "kkharji/sqlite.lua",           module = "sqlite" },
+        { "nvim-telescope/telescope.nvim" },
+    },
+    event = "VeryLazy",
+    keys = {
+        { "<leader>fy", ":Telescope neoclip<cr>",    desc = "Find yanks",  silent = true },
+        { "<leader>f@", ":Telescope macroscope<cr>", desc = "Find macros", silent = true },
+    },
+    opts = {
+        enable_persistent_history = true,
+        continuous_sync = true,
+        keys = {
+            telescope = {
+                i = {
+                    paste_behind = false,
+                    replay = false,
+                },
+            },
+        },
+    },
+}
