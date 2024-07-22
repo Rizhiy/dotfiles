9df804aabfe5859681a9f88b46831caadaa3adf8
Artem Vasenin <vasart169@gmail.com>
2024-07-22 23:48:13 +0100

    chore(nvim): add some git keybindings

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -1,1 +1,8 @@
-return { "tpope/vim-fugitive" }
+return {
+    "tpope/vim-fugitive",
+    event = "VeryLazy",
+    keys = {
+        { "<leader>gC", ":Git commit<CR>", desc = "Git commit", silent = true },
+        { "<leader>gP", ":Git pull<CR>",   desc = "Git pull",   silent = true },
+    },
+}
