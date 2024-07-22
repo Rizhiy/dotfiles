ee44c927dbdf39e4dfa5c908dae24c805b276c2b
Artem Vasenin <rizhiy@meta.com>
2024-07-22 14:36:12 +0100

    chore(nvim): reduce threshold for large files

--- .config/nvim/lua/rizhiy/utils.lua
+++ .config/nvim/lua/rizhiy/utils.lua
@@ -19,7 +19,7 @@ end
 
 --- @param buf number
 function M.is_large_file(buf)
-    local max_filesize = 100 * 1024 -- 100 KB
+    local max_filesize = 50 * 1024 -- 100 KB
     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
     if ok and stats and stats.size > max_filesize then return true end
     return false
