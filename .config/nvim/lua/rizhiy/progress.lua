e9f4ac8fc03c2faad29863d4267dba550edae529
Artem Vasenin <rizhiy@meta.com>
2024-08-08 10:58:22 +0100

    feat(nvim): add function to run system with spinner

--- .config/nvim/lua/rizhiy/progress.lua
+++ .config/nvim/lua/rizhiy/progress.lua
@@ -6,11 +6,13 @@ ProgressCounter.__index = ProgressCounter
 
 ---@param total number | nil
 ---@param desc string | nil
-function ProgressCounter:init(total, desc)
+---@param no_counter boolean | nil if true don't print count
+function ProgressCounter:init(total, desc, no_counter)
     local bar = {}
     setmetatable(bar, ProgressCounter)
     bar._total = total
     bar._desc = desc
+    bar._no_counter = no_counter
 
     bar._progress = 0
     bar._done = false
@@ -23,7 +25,7 @@ end
 function ProgressCounter:_update()
     local message = self._done and "Done" or _spinner[self._spinner_idx]
     if self._desc then message = message .. " " .. self._desc end
-    message = message .. (" %d"):format(self._progress)
+    if not self._no_counter then message = message .. (" %d"):format(self._progress) end
     if self._total then message = message .. ("/%d"):format(self._total) end
 
     self._notif = vim.notify(message, vim.log.levels.INFO, {
@@ -46,8 +48,26 @@ end
 
 function ProgressCounter:close() self._done = true end
 
-local M = {}
+---@param command string[]
+---@param desc string spinner text
+function run_system_with_sinner(command, desc)
+    local all_buffers = vim.api.nvim_list_bufs()
+    local change_modifiable = require("rizhiy.utils").change_modifiable
 
-M.ProgressCounter = ProgressCounter
+    change_modifiable(all_buffers, false)
+    local counter = ProgressCounter:init(nil, desc, true)
+    vim.system(command, { text = true }, function(obj)
+        if obj.code == 0 then
+            vim.notify(obj.stdout)
+        else
+            vim.notify(obj.stderr, vim.log.levels.ERROR)
+        end
+        change_modifiable(all_buffers, true)
+        counter:close()
+    end)
+end
 
-return M
+return {
+    ProgressCounter = ProgressCounter,
+    run_system_with_sinner = run_system_with_sinner,
+}
