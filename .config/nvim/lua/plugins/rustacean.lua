2889e2f9dffb05197d1eaa153bac14eff6fdfea7
Artem Vasenin <vasart169@gmail.com>
2025-01-06 01:26:04 +0000

    fix(nvim): automatically install rust-analyzer

--- .config/nvim/lua/plugins/rustacean.lua
+++ .config/nvim/lua/plugins/rustacean.lua
@@ -2,6 +2,7 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^5", -- Recommended
     lazy = false, -- This plugin is already lazy
+    build = "rustup component add rust-analyzer",
     config = function()
         vim.g.rustaceanvim = {
             server = {
