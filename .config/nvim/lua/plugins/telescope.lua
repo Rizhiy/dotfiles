b1c78db66aed90239ca6bc09305f38887b356e8e
Artem Vasenin <vasart169@gmail.com>
2025-12-27 02:04:35 +0100

    chore(nvim): clean up telescope extensions

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,21 +1,18 @@
 return {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
     dependencies = {
         "nvim-lua/plenary.nvim",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
         "nvim-telescope/telescope-ui-select.nvim",
-        { "stevearc/aerial.nvim",                     opts = {} },
+        { "stevearc/aerial.nvim", opts = {} },
         "nvim-telescope/telescope-github.nvim",
-        "SalOrak/whaler",
         "nvim-telescope/telescope-file-browser.nvim",
-        "piersolenski/telescope-import.nvim",
         "Myzel394/jsonfly.nvim",
         "LintaoAmons/scratch.nvim",
         "rcarriga/nvim-notify",
     },
     keys = {
-        { "<leader>f",   function() FuzzyFindFiles() end,  desc = "Fuzzy search" },
+        { "<leader>f", function() FuzzyFindFiles() end, desc = "Fuzzy search" },
         {
             "<leader>ff",
             function()
@@ -27,24 +24,22 @@ return {
             end,
             desc = "Search files",
         },
-        { "<leader>fe",  ":Telescope live_grep<CR>",       desc = "Exact search",                 silent = true },
-        { "<leader>fb",  ":Telescope buffers<CR>",         desc = "Search buffers",               silent = true },
-        { "<leader>fh",  ":Telescope help_tags<CR>",       desc = "Search help tags",             silent = true },
-        { "<leader>fk",  ":Telescope keymaps<CR>",         desc = "Search keys",                  silent = true },
-        { "<leader>fd",  ":Telescope diagnostics<CR>",     desc = "Search diagnostics",           silent = true },
-        { "<leader>fc",  ":Telescope aerial<CR>",          desc = "Search code parts",            silent = true },
-        { "<leader>fp",  ":Telescope whaler<CR>",          desc = "Search projects",              silent = true },
-        { "<leader>fi",  ":Telescope import<CR>",          desc = "Search for import",            silent = true },
-        { "<leader>fj",  ":Telescope jsonfly<CR>",         desc = "Search json keys",             silent = true },
-        { "<leader>fl",  ":Telescope resume<CR>",          desc = "Resume last search",           silent = true },
-        { "<leader>fq",  ":Telescope quickfix<CR>",        desc = "Search quickfix",              silent = true },
-        { "<leader>fm",  ":Telescope notify<CR>",          desc = "Search messages",              silent = true },
+        { "<leader>fe", ":Telescope live_grep<CR>", desc = "Exact search", silent = true },
+        { "<leader>fb", ":Telescope buffers<CR>", desc = "Search buffers", silent = true },
+        { "<leader>fh", ":Telescope help_tags<CR>", desc = "Search help tags", silent = true },
+        { "<leader>fk", ":Telescope keymaps<CR>", desc = "Search keys", silent = true },
+        { "<leader>fd", ":Telescope diagnostics<CR>", desc = "Search diagnostics", silent = true },
+        { "<leader>fc", ":Telescope aerial<CR>", desc = "Search code parts", silent = true },
+        { "<leader>fj", ":Telescope jsonfly<CR>", desc = "Search json keys", silent = true },
+        { "<leader>fl", ":Telescope resume<CR>", desc = "Resume last search", silent = true },
+        { "<leader>fq", ":Telescope quickfix<CR>", desc = "Search quickfix", silent = true },
+        { "<leader>fm", ":Telescope notify<CR>", desc = "Search messages", silent = true },
 
-        { "<leader>gc",  ":Telescope git_bcommits<CR>",    desc = "Show commit history (buffer)", silent = true },
-        { "<leader>gb",  ":Telescope git_branches<CR>",    desc = "Show all available branches",  silent = true },
-        { "<leader>ghi", ":Telescope gh issues<CR>",       desc = "Show GitHub issues",           silent = true },
-        { "<leader>ghp", ":Telescope gh pull_request<CR>", desc = "Show GitHub pull requests",    silent = true },
-        { "<leader>ghr", ":Telescope gh run<CR>",          desc = "Show GitHub workflow runs",    silent = true },
+        { "<leader>gc", ":Telescope git_bcommits<CR>", desc = "Show commit history (buffer)", silent = true },
+        { "<leader>gb", ":Telescope git_branches<CR>", desc = "Show all available branches", silent = true },
+        { "<leader>ghi", ":Telescope gh issues<CR>", desc = "Show GitHub issues", silent = true },
+        { "<leader>ghp", ":Telescope gh pull_request<CR>", desc = "Show GitHub pull requests", silent = true },
+        { "<leader>ghr", ":Telescope gh run<CR>", desc = "Show GitHub workflow runs", silent = true },
         {
             "<leader>/",
             function() require("telescope.builtin").current_buffer_fuzzy_find() end,
@@ -57,7 +52,6 @@ return {
         telescope.load_extension("ui-select")
         telescope.load_extension("aerial")
         telescope.load_extension("gh")
-        telescope.load_extension("import")
         telescope.load_extension("jsonfly")
         -- telescope.load_extension("noice")
         telescope.load_extension("notify")
@@ -84,17 +78,6 @@ return {
                     },
                 },
             },
-            extensions = {
-                whaler = {
-                    directories = { "~/projects" },
-                    auto_cwd = false,
-                    file_explorer = "telescope_file_browser",
-                    theme = {
-                        previwer = true,
-                    },
-                },
-            },
         })
-        telescope.load_extension("whaler")
     end,
 }
