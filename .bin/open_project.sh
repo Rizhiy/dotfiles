9cfa9831dbc873f7b3fa215c24d8e346128ae9b4
Artem Vasenin <vasart169@gmail.com>
2025-12-27 04:21:44 +0100

    feat: improve open_project.sh

--- .bin/open_project.sh
+++ .bin/open_project.sh
@@ -15,8 +15,25 @@ fi
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
+detect_project_type() {
+    if [[ -f "$selected/pyproject.toml" ]]; then
+        echo "python"
+    elif [[ -f "$selected/Cargo.toml" ]]; then
+        echo "rust"
+    else
+        echo "Could not detect project type for $selected" >&2
+        echo -e "python\nrust" | fzf --prompt="Select project type: "
+    fi
+}
+
 new_window_cmd() {
-    tmux send-keys -t main:$selected_name "cd $selected; act; vim; clear" C-m
+    project_type=$(detect_project_type)
+
+    if [[ $project_type == "python" ]]; then
+        tmux send-keys -t main:$selected_name "cd $selected; act; vim; clear" C-m
+    else
+        tmux send-keys -t main:$selected_name "cd $selected; vim; clear" C-m
+    fi
 }
 
 if [[ -z $tmux_running ]] || ! tmux has-session -t main:$selected_name 2>/dev/null; then
