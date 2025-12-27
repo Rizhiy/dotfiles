67679f8a2f94ef81a7520c388470d7388ccf9027
Artem Vasenin <vasart169@gmail.com>
2025-12-27 17:00:37 +0100

    fix(neovim): fix auto-format for rust

--- .config/nvim/lua/plugins/rustacean.lua
+++ .config/nvim/lua/plugins/rustacean.lua
@@ -1,12 +1,21 @@
 return {
     "mrcjkb/rustaceanvim",
-    version = "^5", -- Recommended
+    version = "^6", -- Recommended
     lazy = false, -- This plugin is already lazy
     build = "rustup component add rust-analyzer",
     config = function()
         vim.g.rustaceanvim = {
             server = {
-                on_attach = require("rizhiy.utils").on_attach,
+                on_attach = function(client, bufnr)
+                    require("rizhiy.utils").on_attach(client, bufnr)
+                    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function() vim.lsp.buf.format({ async = false }) end,
+                    })
+                end,
             },
         }
     end,
