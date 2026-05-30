31387ef0d7e2e3b29e514ee0847def2ab1f1931f
Artem Vasenin <vasart169@gmail.com>
2026-05-31 01:44:16 +0200

    fix(arch): add wallpaper script

---
+++ .bin/set_time_wallpaper_sway.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Set wallpaper based on time
+set_time_wallpaper() {
+	declare -a imgs
+	for img in $HOME/.local/share/Lakeside_Louis_Coyle/*; do
+		imgs=("${imgs[@]}" "$img")
+	done
+	imgs=($(printf "%s\n" ${imgs[@]} | sort -V))
+	hour=$(date +'%H')
+	idx=$(( "10#$hour" / 2 ))
+	swaymsg output "*" bg "${imgs[$idx]}" fill
+}
+set_time_wallpaper
